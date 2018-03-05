b9b89dbe434b8429ddcdf9d91e3add1624fca3c7
Elias Ortiz <elias.ortiz@colorado.edu>
2018-03-04 21:54:06 -0700

    checkpoint for getting polished POC for tomorrow. facebook api returning stuff right now

--- index.js
+++ index.js
@@ -7,10 +7,29 @@
 * @param {Object} req Cloud Function request context.
 * @param {Object} res Cloud Function response context.
 */
+
+var express = require('express');
+var app = express();
+var FB = require('fb');
+
 exports.MessengerBot = function MessengerBot (req, res) {
-    var response = req.body.result.resolvedQuery;
-    res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-    res.send(JSON.stringify({ "speech": response, "displayText": response 
-    //"speech" is the spoken version of the response, "displayText" is the visual version
-    }));
+    var response;
+    FB.setAccessToken('EAACEP1ZBqcxgBABbFdldMmtZC0XDlz33mLr4pOYdhK4LZB6qwZAARZBa16ABo8fPZAwtsLfci5nKmQEO7PRMzbM9MtPG4VbdQfBlRrt1WXmM6QB9jXQbgZAjPRMZCAIoUGD9JoqQj7kHR7v6UscpMQlbh8UcovuAjOwD9O2JixVTOVOyKd3VHnThlUoGF1ufkYXvDBeh5SV0W3VR71Arl76bl0XPM8SSEw25w5wNmZCvSTgZDZD');
+  	FB.api(
+      '/127204964763192/friends',
+      'GET',
+      {},
+      function(response1) {
+          console.log(response1);
+          response = response1.summary.total_count;
+          console.log(response);
+          console.log("OUTSIDE: ", response);
+          //var response = "This is a sample response from your webhook!"
+          res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+          console.log("hi SDF,.VKSJDHGLKSDFJHGLKJHLK");
+          res.send(JSON.stringify({ "speech": response, "displayText": response
+          //"speech" is the spoken version of the response, "displayText" is the visual version
+          }));
+      }
+    );
   };
